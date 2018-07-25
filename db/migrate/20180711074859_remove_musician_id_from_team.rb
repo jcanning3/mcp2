42e4a66cf03b702b5c050e3f9decaca98fd80647
John Canning <john@pcc.com>
2018-07-25 04:34:37 +0200

    keep trying

--- db/migrate/20180711074859_remove_musician_id_from_team.rb
+++ db/migrate/20180711074859_remove_musician_id_from_team.rb
@@ -1,7 +1,5 @@
-=begin
 class RemoveMusicianIdFromTeam < ActiveRecord::Migration[5.2]
   def change
-    remove_column :teams, :musician_id, :integer
+    #remove_column :teams, :musician_id, :integer
   end
 end
-=end
