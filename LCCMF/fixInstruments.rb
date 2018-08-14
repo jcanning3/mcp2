0b48d924c04ae89d875611f376f80fe24a59283f
John Canning <john@pcc.com>
2018-08-13 22:09:09 -0400

    uncomment the line that actually updates instrument_id in fixInstruments.rb

--- LCCMF/fixInstruments.rb
+++ LCCMF/fixInstruments.rb
@@ -2,6 +2,6 @@ Instrument.all.each do |i|
   puts i.name
   Musician.where(old_instrument: i.name.downcase).each do |m|
     puts "  " + m.name
-    #m.update(instrument_id: i.id)
+    m.update(instrument_id: i.id)
   end
 end
