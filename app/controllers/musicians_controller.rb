d4f4b24fda31ab8de1a7d179b453d23fed2c4434
John Canning <john@pcc.com>
2019-07-31 06:21:08 -0400

    2019 Musician list

--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -8,12 +8,14 @@ class MusiciansController < ApplicationController
     @musicians = Musician.all
     @accomodations = Accomodation.all
     @events = Event.all
+    @festivals = Festival.all
   end
 
   # GET /musicians/1
   # GET /musicians/1.json
   def show
     @accomodations = Accomodation.all
+    @festivals = Festival.all
   end
 
   # GET /musicians/new
@@ -74,7 +76,7 @@ class MusiciansController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def musician_params
       params.require(:musician).permit(:name, :first, :last, :instrument_id, :old_instrument, :notes,
-        sheets_attributes: [:id, :_destroy, :note],
+        sheets_attributes: [:id, :_destroy, :note], 
 	team_musicians_attributes: [:id, :_destroy ] )
     end
 end
