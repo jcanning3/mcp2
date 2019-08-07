421aa528b01d23a49c6e77fdc04b0309cf3903f2
John Canning <john@pcc.com>
2019-08-07 04:00:41 -0400

    add location.buttonHtml

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,22 @@
 class Location < ApplicationRecord
   belongs_to :address
   has_many :events
+
+  attr_reader :htmlButton
+
+
+  def htmlButton
+    if ( self.address.present? && self.address.address1 )
+      x = " <button class='btn btn-basic btn-sm'> " +
+          "<a href=" + self.address.mapURL + "> " +
+          self.name + " " +
+          "<span class='fas fa-directions'></span> " +
+          "</a> " +
+          "</button>"
+    else
+      x = self.name
+    end
+    return x.html_safe
+  end
+
 end
