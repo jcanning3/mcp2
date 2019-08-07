c725c050a0f6c832b576b47c937f0aaf218b7215
John Canning <john@pcc.com>
2019-08-07 03:43:46 -0400

    add address.htmlButton

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,12 +3,26 @@ class Address < ApplicationRecord
   has_many :locations
   has_many :musicians, through: :accomodations
 
-  attr_reader :mapURL
+  attr_reader :mapURL, :htmlButton
 
   def mapURL
     x = self.address1 + ' ' + self.city + ' ' + self.state
     x = URI.encode( "http://maps.apple.com/?q=" + x)
     return x
+  end
+
+  def htmlButton
+    if ( self.present? && self.address1 )
+      x = "<button class='btn btn-info btn-sm'> " +
+          "<a href=" + self.mapURL + "> " +
+          self.nickname + " " +
+          "<span class='fas fa-directions'></span> " +
+          "</a> " +
+          "</button>"
+    else
+      x = self.name
+    end 
+    return x.html_safe
   end
 
 end
